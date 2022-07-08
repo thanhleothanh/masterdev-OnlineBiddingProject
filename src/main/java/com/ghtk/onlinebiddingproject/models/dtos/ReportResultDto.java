5ff9422041337ba975083d253079ba4d4864c0eb
thanhleothanh <thanhng260588@gmail.com>
2022-07-08 11:49:38 +0700

    auction api implemented, admin auction api, item/itemimage endpoints added, bid api endpoints, mysql event set up for automatically handle auction status, auctions/winner cron jobs, report/reportimage endpoints added

--- src/main/java/com/ghtk/onlinebiddingproject/models/dtos/ReportResultDto.java
+++ src/main/java/com/ghtk/onlinebiddingproject/models/dtos/ReportResultDto.java
@@ -1,21 +1,22 @@
 package com.ghtk.onlinebiddingproject.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ghtk.onlinebiddingproject.constants.ReportResultConstants;
+import com.ghtk.onlinebiddingproject.models.entities.Report;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class ReportResultDto {
     private Integer id;
 
-    @NotBlank(message = "thiếu thông tin kết quả (REJECTED/ACCEPTED)!")
+    @NotNull(message = "thiếu thông tin kết quả (REJECTED/ACCEPTED)!")
     private ReportResultConstants result;
 
-    @NotNull(message = "thiếu thông tin phiếu báo cáo!")
-    private ReportDto report;
+    @JsonIgnore
+    private Report report;
 
-    @NotNull(message = "thiếu thông tin admin!")
+    @JsonIgnore
     private AdminDto admin;
 }
--- src/main/java/com/ghtk/onlinebiddingproject/utils/converters/DtoToEntityConverter.java
+++ src/main/java/com/ghtk/onlinebiddingproject/utils/converters/DtoToEntityConverter.java
@@ -1,9 +1,10 @@
 package com.ghtk.onlinebiddingproject.utils.converters;
 
-import com.ghtk.onlinebiddingproject.models.dtos.AuctionDto;
-import com.ghtk.onlinebiddingproject.models.dtos.ItemDto;
-import com.ghtk.onlinebiddingproject.models.entities.Auction;
-import com.ghtk.onlinebiddingproject.models.entities.Item;
+import com.ghtk.onlinebiddingproject.models.dtos.*;
+import com.ghtk.onlinebiddingproject.models.entities.*;
+import com.ghtk.onlinebiddingproject.models.requests.AuctionRequestDto;
+import com.ghtk.onlinebiddingproject.models.requests.BidRequestDto;
+import com.ghtk.onlinebiddingproject.models.requests.ItemRequestDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.expression.ParseException;
@@ -11,15 +12,40 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DtoToEntityConverter {
+
     @Autowired
-    ModelMapper modelMapper;
+    private ModelMapper modelMapper;
 
     public Auction convertToEntity(AuctionDto auctionDto) throws ParseException {
         return modelMapper.map(auctionDto, Auction.class);
     }
 
-    public Item convertToEntity(ItemDto itemDto) throws ParseException {
+    public Auction convertToEntity(AuctionRequestDto auctionDto) throws ParseException {
+        return modelMapper.map(auctionDto, Auction.class);
+    }
+
+    public Item convertToEntity(ItemRequestDto itemDto) throws ParseException {
         return modelMapper.map(itemDto, Item.class);
+    }
+
+    public Report convertToEntity(ReportDto reportDto) throws ParseException {
+        return modelMapper.map(reportDto, Report.class);
+    }
+
+    public ReportResult convertToEntity(ReportResultDto reportResultDto) throws ParseException {
+        return modelMapper.map(reportResultDto, ReportResult.class);
+    }
+
+    public ItemImage convertToEntity(ItemImageDto itemImageDto) throws ParseException {
+        return modelMapper.map(itemImageDto, ItemImage.class);
+    }
+
+    public ReportImage convertToEntity(ReportImageDto reportImageDto) throws ParseException {
+        return modelMapper.map(reportImageDto, ReportImage.class);
+    }
+
+    public Bid convertToEntity(BidRequestDto bidDto) throws ParseException {
+        return modelMapper.map(bidDto, Bid.class);
     }
 
 
